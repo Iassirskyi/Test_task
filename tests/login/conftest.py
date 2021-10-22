845312773e0ff59ac5e69af25d88b0fda8c6f146
Iassirskyi <s.iassirskyi@gmail.com>
2021-10-22 14:16:50 +0300

    update fixture

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 
+from framework.login_page import LoginPage
+
 
 @pytest.fixture(scope='function')
 def user_login_fixture(page_manager):
-    yield page_manager
+    login_page = page_manager.create_page(LoginPage)
+    yield login_page
