a0c6ea1f4105b7679820f35279f0efe90129f039
Iassirskyi <s.iassirskyi@gmail.com>
2021-10-24 14:59:04 +0300

    added sidebar_fixture

--- tests/login/conftest.py
+++ tests/login/conftest.py
@@ -1,9 +1,17 @@
 import pytest
 
 from framework.login_page import LoginPage
+from framework.sidebar_page import SideBarPage
 
 
 @pytest.fixture(scope='function')
 def user_login_fixture(page_manager):
     login_page = page_manager.create_page(LoginPage)
     yield login_page
+
+
+@pytest.fixture(scope='function')
+def sidebar_fixture(page_manager):
+    sidebar_page = page_manager.create_page(SideBarPage)
+    yield sidebar_page
+
